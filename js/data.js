2bb4615c987a3905da99597fce87a91ed5d0f685
Ying Zhang <dodozhang21@gmail.com>
2013-05-01 16:18:07 -0500

    add data

---
+++ js/data.js
@@ -0,0 +1,218 @@
+var months = 
+{
+    "1": {
+        "models": [
+            {
+                "name": "Kat",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            },
+			{
+                "name": "Jessica",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            }
+        ],
+        "text": "Congratulations! Welcome to the most exciting, personal, and magical journey of your life. As your body makes a home for your baby, note what's happening to your body, especially to your belly. Measure your waist and take a photo of yourself each month – you'll have fun comparing later!"
+    },
+	
+    "2": {
+        "models": [
+            {
+                "name": "Kat",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            },
+			{
+                "name": "Jessica",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            }
+        ],
+        "text": "Don't you love having a secret? Like many other expectant parents, you might choose to hold off on the big announcement. If so, this time is full of meaningful glances at your partner and quiet moments of joy. Cherish it as you prepare for a whole new life."
+    },
+	
+    "3": {
+        "models": [
+            {
+                "name": "Kat",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            },
+			{
+                "name": "Jessica",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            }
+        ],
+        "text": "As your first trimester comes to a close, the reality of pregnancy and parenthood often hits. Remember, by treating yourself well you're also creating the best environment for your growing baby. Pamper yourself physically and emotionally; you and your baby deserve it!"
+    },
+	
+    "4": {
+        "models": [
+            {
+                "name": "Kat",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            },
+			{
+                "name": "Jessica",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            }
+        ],
+        "text": "During these middle three months, your growing belly will become more obvious to the world. Showing is a true rite of passage of pregnancy; take pride in your rounded body and your radiant glow. From maternity clothes to pregnancy privileges, enjoy this special time!"
+    },
+	
+    "5": {
+        "models": [
+            {
+                "name": "Kat",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            },
+			{
+                "name": "Jessica",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            }
+        ],
+        "text": "In two more weeks, you'll be halfway to your due date! Whether time has flown or crawled by, this is a good moment to reflect on your pregnancy experience. It's so important now to stay positive and practice self-care. What's good for you is good for your baby."
+    },
+	
+    "6": {
+        "models": [
+            {
+                "name": "Kat",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            },
+			{
+                "name": "Jessica",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            }
+        ],
+        "text": "Isn't it miraculous to feel your baby kicking? Quickening, feeling him moving inside the uterus, is a highlight event for pregnant women and their partners. Revel in this direct contact with your baby, and see if you can decipher the body part: foot, elbow, head, or backside?"
+    },
+	
+    "7": {
+        "models": [
+            {
+                "name": "Kat",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            },
+			{
+                "name": "Jessica",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            }
+        ],
+        "text": "Remember, there is no right shape for your growing belly. Some women carry babies high, some low, and some hardly show at all. Don't be discouraged if you're a basketball or a pumpkin or a pear. Your shape is perfect, beautiful, and right for you."
+    },
+	
+    "8": {
+        "models": [
+            {
+                "name": "Kat",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            },
+			{
+                "name": "Jessica",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            }
+        ],
+        "text": "The stares, touches, and reactions to your belly right now may catch you off-guard - or drive you crazy! You have a right to your own space, so follow your instincts when it comes to strangers and friends giving unwanted attention."
+    },
+	
+    "8": {
+        "models": [
+            {
+                "name": "Kat",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            },
+			{
+                "name": "Jessica",
+                "angles": [
+                    "Front",
+                    "Back",
+                    "Left",
+                    "Right"
+                ]
+            }
+        ],
+        "text": "Down the home stretch! Your belly is round, resilient, and ready for birth. Your feelings may bounce from excitement to anxiety, but give yourself credit for how far you've come on this amazing nine-month journey. And get set to meet your baby!"
+    }
+}
